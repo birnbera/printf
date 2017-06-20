a9324be7cb850cf20f9f4cbe04838a399427d610
Tanya Kryukova <ttkryukova@gmail.com>
2017-06-20 19:38:59 +0000

    finished hexadecimal functions

---
+++ conv_X.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * conv_X - convert unsigned int argument to upper case hex
+ * @spec: struct containing information on formatting of argument
+ * @ap: variable argument list
+ *
+ * Return: Number of bytes printed
+ */
+int conv_X(spec_t *spec, va_list ap)
+{
+	char s[65], pad;
+	int i, j, tbytes = 0;
+	unsigned long int n = 0;
+
+	if (spec->length == 'l')
+		n = va_arg(ap, unsigned long int);
+	else
+		n = va_arg(ap, unsigned int);
+	i = 0;
+
+	do {
+		if ((n % 16) < 10)
+			s[i] = (n % 16) + '0';
+		else
+			s[i] = ((n % 16) - 10) + 'A';
+		n /= 16;
+		++i;
+		} while (n);
+	s[i] = '\0';
+	j = spec->width - i;
+	if (spec->flags & JUST_FLAG)
+	{
+		while (--i >= 0)
+			write(1, s + i, 1), ++tbytes;
+		while (--j >= 0)
+			write(1, " ", 1), ++tbytes;
+	}
+	else
+	{
+		pad = (spec->flags & ZERO_FLAG ? '0' : ' ');
+		while (--j >= 0)
+			write(1, &pad, 1), ++tbytes;
+		while (--i >= 0)
+			write(1, s + i, 1), ++tbytes;
+	}
+	return (tbytes);
+}
--- set_specs.c
+++ set_specs.c
@@ -135,9 +135,9 @@ int settype(const char *format, spec_t *spec)
  *			{'f', conv_f},
  *			{'e', conv_e},
  *			{'E', conv_E},
- *			{'x', conv_x},
- *			{'X', conv_X},
- *			{'o', conv_o},
+ */			{'x', conv_x},
+			{'X', conv_X},
+/*			{'o', conv_o},
  */
 			{'s', conv_s},
 			{'c', conv_c},
